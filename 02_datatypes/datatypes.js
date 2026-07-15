9e0ec2b959514281c86710ef7c56d19503be2ba8
shubham228-mas <shubham6498g@gmail.com>
2026-07-15 06:46:49 +0000

    second datattypes

---
+++ 02_datatypes/datatypes.js
@@ -0,0 +1,19 @@
+let name = "shubham"
+let age = 25
+let inLoggedIn = true 
+
+//number=> 2 to power 53 
+// big int 
+// boolean
+// strin => ""
+// undefined 
+// null = standalone value means this is not undefined but this is empty
+// symbol=>  unique 
+
+
+// objects
+
+console.log(typeof  "shubham" )//string 
+console.log(typeof  age  )//number 
+console.log(typeof  null ) //object
+console.log(typeof  undefined  )//undefined 
