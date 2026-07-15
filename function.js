e488f82eb37b9c64c06138a3d2a0b72281a2186b
shubham228-mas <shubham6498g@gmail.com>
2026-07-15 17:57:07 +0000

    conersion of datatypes

---
+++ function.js
@@ -0,0 +1,19 @@
+// let score =null
+
+// // console.log(typeof score);
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log( valueInNumber);
+
+
+
+
+// "33"=>33
+//null = 0
+// "33abgg"=>NaN
+
+let someNumber = 23
+
+let stringNumber = string(someNumber )
+console.log(typeof stringNumber)
+console.log(stringNumber)
