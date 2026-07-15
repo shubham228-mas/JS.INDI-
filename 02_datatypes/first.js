ed74a034f28e4dba9084edd7113c8a820995a68e
shubham228-mas <shubham6498g@gmail.com>
2026-07-15 06:48:52 +0000

    third datatype

---
+++ 02_datatypes/first.js
@@ -0,0 +1,13 @@
+const accountId = 228228
+let accountEmail = "shubham@google.com"
+var accountPassword = "334533"
+let accountState;
+// accountId = 2 //cannt ne changes cont variable 
+
+accountEmail = "sh.@sh.com"
+accountPassword = "765456"
+accountCity = "bengluru "
+
+console.log(accountEmail);
+
+console.table([accountId , accountEmail, accountPassword , accountCity,accountState  ]);
