5cd031e62881e624d09c471b9b11a1ceec3dbc6e
shubham228-mas <shubham6498g@gmail.com>
2026-07-16 07:54:26 +0000

    conversion

--- function.js
+++ function.js
@@ -1,30 +1,55 @@
- let score =null
+//  let score =null
 
- console.log(typeof score);
- let valueInNumber = Number(score)
- console.log(typeof valueInNumber);
- console.log( valueInNumber);
-
-
+//  console.log(typeof score);
+//  let valueInNumber = Number(score)
+//  console.log(typeof valueInNumber);
+//  console.log( valueInNumber);
 
 
-// "33"=>33
-//null = 0
-// "33abgg"=>NaN
-
-let someNumber = 23
-
-let stringNumber = String(someNumber )
-console.log(typeof stringNumber)
- console.log(stringNumber)
 
 
-let string = "shubham bansal "
+// // "33"=>33
+// //null = 0
+// // "33abgg"=>NaN
 
-let booleanNumber = Boolean(string)
-console.log(typeof booleanNumber)
-console.log(booleanNumber )
+// let someNumber = 23
+
+// let stringNumber = String(someNumber )
+// console.log(typeof stringNumber)
+//  console.log(stringNumber)
+
+
+// let string = "shubham bansal "
+
+// let booleanNumber = Boolean(string)
+// console.log(typeof booleanNumber)
+// console.log(booleanNumber )
 
 // 1 =>true 0 =>false
 // "" =>false 
 // "shubham bansal " =>true 
+
+//********operation***************88888 */
+
+let value = 3
+let negValue = -value
+
+// console.log(negValue);
+// console.log("1"+ 2 );
+// console.log(1+ "2" );
+// console.log("1"+ 2 + 2  );
+// console.log(1+ 2+"2" );
+
+
+//console.log(+ true  );//gives value 1 
+// console.log(+ "" );//gives value 0
+
+let gameCounter = 1000
+
+ gameCounter++;
+  ++gameCounter;
+ gameCounter--;
+  --gameCounter;
+
+console.log(gameCounter);
+
