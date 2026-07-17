cb8d766a5b8f16ab1fc6e15ac8eeb164e6968c47
shubham228-mas <shubham6498g@gmail.com>
2026-07-17 05:35:28 +0000

    summary of datatyypes

---
+++ datatypes_summary.js
@@ -0,0 +1,37 @@
+// PRIMITIVE 
+
+// 7 TYPES : STRING , BOOLEAN , NUMBER , NULL , UNDEFINED , SUMBOL , BIGINT 
+
+const score = 100
+const scoreValue  = 100.8//type of number 
+
+const isLoggedIn = true 
+const outSideTemp = null// type of null = object ayega 
+
+let userEmail ;
+
+const id = Symbol('123')
+const anotherId = Symbol('123') //type of symbol
+
+console.log(id === anotherId )
+
+// const bigNumber  = 38405034975034750  number if at the end is not n if n then big int 
+
+
+
+ const heroes = [" shoto ","wolvon","kd","sahiljaatt "]; //type of ibject 
+let myObj= {
+
+    name : "shubham",
+    age : 34,
+}
+
+const myFunction = function(){ //type of function 
+
+    console.log( "Hello World ");
+}
+
+console.log(typeof heroes  )
+// REFERANCE TYPE OR (NON PRIMITIVE )
+
+//  Array , objects , functions 
