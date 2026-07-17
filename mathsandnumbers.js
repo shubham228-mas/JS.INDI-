e6cc193adc8bae2405df5261a020029b735fbd72
shubham228-mas <shubham6498g@gmail.com>
2026-07-17 17:56:29 +0000

    numbers and maths

---
+++ mathsandnumbers.js
@@ -0,0 +1,40 @@
+// const score = 400
+// console.log(score)
+
+// const balance = new Number(300)
+// console.log(balance)
+
+// console.log(balance.toString().length)
+// console.log(balance.toFixed(4))
+
+// const otherNumber = 276.47668
+// console.log(otherNumber.toPrecision(4));
+
+// const hundreds = 10000000000000000
+
+// console.log(hundreds.toLocaleString('en-IN'));//max and min numbers are also there according to javascript 
+
+
+//*****************maths ****************log
+
+console.log(Math);
+console.log(Math.abs(-8));
+console.log(Math.round(6.5));
+console.log(Math.ceil(5.5));
+console.log(Math.floor(5.7));
+console.log(Math.min(4,5,7,2,5,));
+console.log(Math.max(5,7,3,4,2,4));
+
+
+console.log(Math.random());// alwyas gives random number between 0 to 1 
+console.log((Math.random()*10) + 1);//then we multiply with 10 and add 1 for bettter result 
+console.log(Math.floor(Math.random()*10) + 1);// then we remove decimals buy adding floor 
+
+const max = 20
+const min  = 10
+
+console.log(Math.floor(Math.random()* (max - min + 1 ))+ min)
+ /* here we elaborate my letting 
+max and min variable and here we get no. above then 10 and minimub 10 to chahiye hi chahiye */
+
+
